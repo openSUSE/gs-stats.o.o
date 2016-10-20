74d04dd73aef93cc06756f79a5f3fd721dcbb2af
Dominique Leuenberger <dimstar@opensuse.org>
2016-10-20 10:52:15 +0000

    appdata-failed is no longer provided

--- cron.d/update-AS-trends.sh
+++ cron.d/update-AS-trends.sh
@@ -9,10 +9,10 @@ SNAPSHOT=$(python get-snapshot-version.py)
 cp data.txt data.txt.tmp
 rm appdata.html.xz appdata.xml.gz appdata-failed.xml.gz 
 wget http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.xml.gz
-wget http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata-failed.xml.gz
+#wget http://download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata-failed.xml.gz
 appstream-util status-html appdata.xml.gz appdata.html
 appstream-util matrix-html matrix-view.html appdata.xml.gz
-appstream-util status-html appdata-failed.xml.gz appdata-failed.html
+#appstream-util status-html appdata-failed.xml.gz appdata-failed.html
 
 python update-stats.py >> data.txt.tmp
 
