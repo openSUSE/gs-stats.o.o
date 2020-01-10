0621dcc9d529aa9e419de5ac52b5ddc41f9cd638
Dominique Leuenberger <dimstar@opensuse.org>
2020-01-10 13:35:14 +0100

    Silence a warning about untracked files

--- appstream-trend/update-appdata_html.sh
+++ appstream-trend/update-appdata_html.sh
@@ -9,6 +9,6 @@ pushd $(dirname $0)
 #unxz appdata.html.xza
 
 # We already downloaded the .xz file earlier and decompressed it
-git commit appdata.html data.txt appdata-failed.html matrix-view.html -m "appdata.html: $1" --no-gpg-sign
 rm appdata.xml.gz
+git commit appdata.html data.txt appdata-failed.html matrix-view.html -m "appdata.html: $1" --no-gpg-sign
 git push
