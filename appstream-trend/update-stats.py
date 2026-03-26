2a263639394e93fa687c330d8335d7a0e23a0e85
Dominique Leuenberger <dimstar@opensuse.org>
2026-03-26 12:51:21 +0100

    refactor(appstream): fix python syntax warning and improve file handling
    Updates the regex pattern to a raw string to resolve Python 3.12
    invalid escape sequence warnings. Additionally, wraps the file
    read in a context manager for better resource management.

--- appstream-trend/update-stats.py
+++ appstream-trend/update-stats.py
@@ -4,8 +4,9 @@ import re
 import sys
 
 APPDATA="appdata.html"
-APPCNT = open(APPDATA, 'r').read()
-DATA = re.findall('<tr><td class="alt">Keywords</td><td>(\d+)/(\d+)</td><td class="thin">.*</td></tr>', APPCNT)
+with open(APPDATA, 'r') as f:
+    APPCNT = f.read()
+DATA = re.findall(r'<tr><td class="alt">Keywords</td><td>(\d+)/(\d+)</td><td class="thin">.*</td></tr>', APPCNT)
 
 print ("%s:%s" % (sys.argv[1], DATA[0][1]))
 
